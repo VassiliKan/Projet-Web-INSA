568255c3a046c49a85009f91429e9cc49d7f7d7c
Sachoman fou <sachadarthenucq@gmail.com>
2021-12-06 10:31:44 +0100

    css ++

--- header.js
+++ header.js
@@ -6,7 +6,8 @@ headerprint = function(){
     let date = JSON.parse(sessionStorage.getItem("date"));
     document.getElementById("argent_courant2").innerHTML = Math.round(argent);
     document.getElementById("dettes").innerHTML = Math.round(dettes);
-    document.getElementById("date_jeu").innerHTML = date.jour +" / " + date.mois + " / " + date.annee + " " + "heure : " + date.heure +"h";
+    document.getElementById("date_jeu").innerHTML = " " + date.jour +" / " + date.mois + " / " + date.annee + " ";
+    document.getElementById("heure_jeu").innerHTML = " " + date.heure +"h";
 }
 
 document.addEventListener('DOMContentLoaded', function() {
